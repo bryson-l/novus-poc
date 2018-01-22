b194737f26b74f348f8642b414bf05a5da7895d5
Bryson <bryson326@gmail.com>
2018-01-22 18:27:55 -0500

    porting project

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import * as $ from 'jquery'
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'app';
+  ngOnInit() {
+    $(document).ready(function () {
+      var trigger = $('.hamburger'),
+          overlay = $('.overlay'),
+         isClosed = false;
+    
+        trigger.click(function () {
+          hamburger_cross();      
+        });
+    
+        function hamburger_cross() {
+    
+          if (isClosed == true) {          
+            overlay.hide();
+            trigger.removeClass('is-open');
+            trigger.addClass('is-closed');
+            isClosed = false;
+          } else {   
+            overlay.show();
+            trigger.removeClass('is-closed');
+            trigger.addClass('is-open');
+            isClosed = true;
+          }
+      }
+      
+      $('[data-toggle="offcanvas"]').click(function () {
+            $('#wrapper').toggleClass('toggled');
+      });  
+    });
+  }
+}
